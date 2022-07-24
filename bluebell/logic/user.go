5bfd49bc7e57318f3299a99572c556d80c88f626
xuanbiyijue <97445238+xuanbiyijue@users.noreply.github.com>
2022-07-24 10:08:32 +0800

    Add files via upload

---
+++ bluebell/logic/user.go
@@ -0,0 +1,44 @@
+package logic
+
+import (
+	"bluebell/dao/mysql"
+	"bluebell/models"
+	"bluebell/pkg/jwt"
+	"bluebell/pkg/snowflake"
+)
+
+func SignUp(p *models.ParamSignUp) (err error) {
+	// 判断用户是否存在
+	err = mysql.CheckUserExist(p.Username)
+	if err != nil {
+		return err
+	}
+	// 生成UID
+	userID := snowflake.GenID()
+	// 构造一个user实例
+	user := &models.User{
+		UserID:   userID,
+		Username: p.Username,
+		Password: p.Password,
+	}
+	// 保存进数据库
+	return mysql.InsertUser(user)
+}
+
+func Login(p *models.ParamLogin) (user *models.User, err error) {
+	user = &models.User{
+		Username: p.Username,
+		Password: p.Password,
+	}
+	// 传递的是指针，就能拿到user.UserID
+	if err := mysql.Login(user); err != nil {
+		return nil, err
+	}
+	// 生成JWT
+	token, err := jwt.GenToken(user.UserID, user.Username)
+	if err != nil {
+		return
+	}
+	user.Token = token
+	return
+}
